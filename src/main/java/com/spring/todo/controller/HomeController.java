11ceafeeadb2b1344207fabaae052e4938fd4b0c
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-03-16 22:37:01 +0530

    Initial project setup

---
+++ src/main/java/com/spring/todo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.spring.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+  @RequestMapping("/")
+  public String index() {
+    System.out.println("index");
+    return "index"; }
+}
